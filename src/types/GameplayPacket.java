94d28a49ef42673a0117fd4aad82c3647ad5d526
Cuppy130 <player130games@gmail.com>
2024-10-28 00:35:41 -0700

    only use what we need

--- src/types/GameplayPacket.java
+++ src/types/GameplayPacket.java
@@ -5,10 +5,6 @@ public class GameplayPacket {
     public static final int PlayerLeft = 0x07;
     public static final int PlayerUpdate = 0x08;
     public static final int Chat = 0x09;
-    public static final int ServerRequestingItem = 0x0A;
-    public static final int PlayerUsername = 0x0B;
-    public static final int Activate = 0x0C;
-    public static final byte GetPlayers = 0x0D;
 }
 
 
